9f0cbf43f025651aeee3ebfc6a021fcfbb22d566
sharvan mmodi <sharvanmodi900@gmail.com>
2024-09-08 18:46:17 +0530

    build release

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+  
+//   plugins: [react()],
+//   server:{
+//     host: '0.0.0.0',
+//     port: 3000,
+
+  
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  
+  base: '/xt5/',  // Add this line with your repository name
   plugins: [react()],
-  server:{
+  server: {
     host: '0.0.0.0',
     port: 3000,
-
-  
-  }
+  },
 })
+
